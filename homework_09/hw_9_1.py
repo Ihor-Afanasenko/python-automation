9f449ac451a42d98dd65b7f36fd1013679787c34
= <nikita.limarchuk@gmail.com>
2021-11-17 10:30:07 +0200

    Add some changes in annotations

--- homework_09/hw_9_1.py
+++ homework_09/hw_9_1.py
@@ -1,9 +1,13 @@
-def arithmetic(first_number: float, second_number: float, operation: str) -> float:
+from typing import Union
+
+
+def arithmetic(first_number: float, second_number: float, operation: str) -> Union[float, str]:
     """
     Function arithmetic gets two number and operation ('+', '-', '*', '/')
     return result operation with two number or
     if operation not correct message 'Not known operation: {operation}'
     """
+
     if operation == '+':
         result = first_number + second_number
     elif operation == '-':
@@ -14,4 +18,5 @@ def arithmetic(first_number: float, second_number: float, operation: str) -> flo
         result = first_number / second_number
     else:
         result = f"Not known operation: {operation}"
+    
     return result
