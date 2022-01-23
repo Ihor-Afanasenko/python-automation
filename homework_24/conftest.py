ef57caf782fa7a26f54c197f8d6d622e19b8a13a
Ihor Afanasenko <ihor.afanasenko.work@gmail.com>
2022-01-23 22:26:29 +0200

    added fixes

--- homework_24/conftest.py
+++ homework_24/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
 from .core.config import Config
 from homework_24.pages import LandingPage
 
@@ -12,10 +11,7 @@ def config() -> Config:
 
 @pytest.fixture(scope="session")
 def driver(config) -> Chrome:
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = Chrome(config.driver_path, chrome_options=chrome_options)
+    driver = Chrome(config.driver_path)
     driver.get(config.host)
     driver.maximize_window()
     yield driver
