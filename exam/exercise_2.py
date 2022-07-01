28554171ca2c5031ed421816b0cf1740bd089e04
Ihor Afanasenko <ihor.afanasenko.work@gmail.com>
2022-07-01 09:44:53 +0300

    added alternative implementation

--- exam/exercise_2.py
+++ exam/exercise_2.py
@@ -10,3 +10,13 @@ def find_even_number_in_list(input_list: [int]) -> list: return [element for ele
 # E.g.
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 print(find_even_number_in_list(a))
+
+
+# Implementation using lambda function
+def find_even_number_in_list_use_lambda(input_list: [int]) -> list:
+    return list(filter(lambda x: (not x & 1), input_list))
+
+
+# E.g.
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print(find_even_number_in_list_use_lambda(a))
